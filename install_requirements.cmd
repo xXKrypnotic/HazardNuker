ff09309b925c6f8a49a869db56cef47fc200469c
Rdimo <rasmus.nyblom@hotmail.com>
2021-10-18 11:37:06 +0200

    added msvcrt lib

--- install_requirements.cmd
+++ install_requirements.cmd
@@ -1,2 +1,2 @@
-pip install requests selenium bs4 colorama undetected_chromedriver tqdm datetime pillow pyinstaller psutil
+pip install requests selenium bs4 colorama undetected_chromedriver tqdm datetime pillow pyinstaller psutil msvcrt
 python Hazard.py
